66122b97225cdb77e39d5efa48392357ec2cc29f
Ferhan Akkan <ferhan.akkan@exoplanet.digital>
2023-01-07 14:29:27 +0300

    Refactor FAUserDefaults

---
+++ Sources/FAFoundation/Constants/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Ferhan Akkan on 7.01.2023.
+//
+
+import Foundation
+
+struct Constants {}
--- Sources/FAFoundation/Managers/FAUserDefaults.swift
+++ Sources/FAFoundation/Managers/FAUserDefaults.swift
@@ -9,53 +9,27 @@ import Foundation
 
 @propertyWrapper
 public struct FAUserDefaults<Value> {
+
     let key: String
 
-    private let userDefaults = UserDefaults(suiteName: "faFoundation")
+    private var userDefaults: UserDefaults {
+        return UserDefaults(suiteName: Constants.FAUserDefaults.suiteName)!
+    }
 
     public var wrappedValue: Value? {
         get {
-            userDefaults?.value(forKey: key) as? Value
+            userDefaults.value(forKey: key) as? Value
         }
 
         set {
             if let value = newValue {
-                userDefaults?.setValue(value, forKey: key)
+                userDefaults.setValue(value, forKey: key)
             } else {
-                userDefaults?.removeObject(forKey: key)
+                userDefaults.removeObject(forKey: key)
             }
         }
     }
-    
-    public init(key: String) {
-        self.key = key
-    }
-}
 
-@propertyWrapper
-public struct FAUserDefaultsCodableValue<Value: Codable> {
-    let key: String
-
-    private let userDefaults = UserDefaults(suiteName: "faFoundation")
-
-    public var wrappedValue: Value? {
-        get {
-            guard let data = userDefaults?.data(forKey: key) else {
-                return nil
-            }
-
-            return try? JSONDecoder().decode(Value.self, from: data)
-        }
-
-        set {
-            if let value = newValue {
-                userDefaults?.set(value.asDictionary, forKey: key)
-            } else {
-                userDefaults?.removeObject(forKey: key)
-            }
-        }
-    }
-    
     public init(key: String) {
         self.key = key
     }
